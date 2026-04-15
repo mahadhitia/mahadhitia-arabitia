5f6b18a17ec145125e9ccb0dacf5d969ad3b2d3d
mahadhitia <muhammadnaufalmahadhitia@gmail.com>
2026-04-16 06:23:34 +0700

    Create topics.js

---
+++ data/topics.js
@@ -0,0 +1,18 @@
+const topics = {
+  kamar: "Kosakata sekitar kamar dan asrama",
+  kamar_mandi: "Kosakata sekitar kamar mandi",
+  sekolah: "Kosakata sekitar kelas dan sekolah",
+  masjid: "Kosakata sekitar masjid",
+  taman: "Kosakata sekitar taman dan lapangan",
+  dapur: "Kosakata sekitar ruang makan dan dapur",
+  pakaian: "Kosakata sekitar pakaian dan perhiasan",
+  profesi: "Kosakata sekitar profesi",
+  pasar: "Kosakata sekitar pasar dan pusat perbelanjaan",
+  kesehatan: "Kosakata sekitar ruang kesehatan",
+  alam: "Kosakata sekitar alam",
+  keluarga: "Kosakata sekitar keluarga",
+  tubuh: "Kosakata sekitar anggota badan",
+  hewan: "Kosakata sekitar nama-nama hewan",
+  alat: "Kosakata sekitar peralatan",
+  transportasi: "Kosakata sekitar transportasi"
+};
