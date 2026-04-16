f053cbff697a23ba3eed64cfc610113b8cbce298
mahadhitia <muhammadnaufalmahadhitia@gmail.com>
2026-04-16 10:26:53 +0700

    Create fiil_topics.js

---
+++ data/fiil_topics.js
@@ -0,0 +1,7 @@
+const fiilTopics = {
+  rumah: "Aktivitas di rumah",
+  sekolah: "Aktivitas di sekolah",
+  ibadah: "Aktivitas ibadah",
+  sosial: "Aktivitas sosial",
+  unclassified: "Belum tersedia"
+};
