d1fecfaf60262a2366c34ea8391fd95f39a8fc2f
mahadhitia <muhammadnaufalmahadhitia@gmail.com>
2026-04-16 11:08:51 +0700

    Update fiil_topics.js

--- data/fiil_topics.js
+++ data/fiil_topics.js
@@ -1,8 +1,4 @@
 const fiilTopics = {
   harian: "Aktivitas sehari-hari",
-  gerak: "Pergerakan & posisi",
-  interaksi: "Interaksi sosial",
-  perasaan: "Perasaan & mental",
-  umum: "Aktivitas umum",
   unclassified: "Belum tersedia"
 };
