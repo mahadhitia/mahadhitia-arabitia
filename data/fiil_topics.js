da5512a71c67e801312a0adbd6d45ae06fcfe3af
mahadhitia <muhammadnaufalmahadhitia@gmail.com>
2026-04-17 18:54:35 +0700

    Update fiil_topics.js

--- data/fiil_topics.js
+++ data/fiil_topics.js
@@ -1,4 +1,35 @@
 const fiilTopics = {
-  harian: "Aktivitas sehari-hari",
-  unclassified: "Belum tersedia"
+  belajar: "Aktivitas akademik (belajar)",
+  evaluasi: "Aktivitas akademik (evaluasi)",
+  mengajar: "Aktivitas akademik (mengajar)",
+
+  arah: "Aktivitas gerakan (arah)",
+  perpindahan: "Aktivitas gerakan (perpindahan)",
+  posisi: "Aktivitas gerakan (posisi)",
+
+  kegiatan_rumah: "Aktivitas harian (kegiatan rumah)",
+  perawatan_diri: "Aktivitas harian (perawatan diri)",
+
+  manipulasi: "Aktivitas interaksi dengan benda (manipulasi)",
+  perawatan_benda: "Aktivitas interaksi dengan benda (perawatan)",
+  perubahan: "Aktivitas interaksi dengan benda (perubahan)",
+
+  hubungan: "Aktivitas interaksi sosial (hubungan)",
+  komunikasi: "Aktivitas interaksi sosial (komunikasi)",
+  transaksi: "Aktivitas interaksi sosial (transaksi)",
+
+  akhlak: "Aktivitas keagamaan (akhlak)",
+  ibadah: "Aktivitas keagamaan (ibadah)",
+  ritual: "Aktivitas keagamaan (ritual)",
+
+  makan_minum: "Aktivitas konsumsi (makan & minum)",
+  memasak: "Aktivitas konsumsi (memasak)",
+  penyajian: "Aktivitas konsumsi (penyajian)",
+
+  berpikir: "Aktivitas mental (berpikir)",
+  keinginan: "Aktivitas mental (keinginan)",
+  perasaan: "Aktivitas mental (perasaan)",
+
+  kompetisi: "Aktivitas olahraga (kompetisi)",
+  permainan: "Aktivitas olahraga (permainan)"
 };
