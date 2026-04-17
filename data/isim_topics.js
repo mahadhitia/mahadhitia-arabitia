2b44e492dc0d1e008d6e0141d46f2f44f12a51c1
mahadhitia <muhammadnaufalmahadhitia@gmail.com>
2026-04-17 18:54:01 +0700

    Update isim_topics.js

--- data/isim_topics.js
+++ data/isim_topics.js
@@ -1,4 +1,43 @@
 const isimTopics = {
-  kamar: "Lingkungan kamar dan asrama",
-  unclassified: "Belum tersedia"
+  bentang_alam: "Kosakata sekitar alam (bentang alam)",
+  material_alam: "Kosakata sekitar alam (material alam)",
+  tumbuhan: "Kosakata sekitar alam (tumbuhan)",
+
+  aksesoris: "Kosakata sekitar barang pribadi (aksesoris)",
+  kebersihan_diri: "Kosakata sekitar barang pribadi (kebersihan diri)",
+  pakaian: "Kosakata sekitar barang pribadi (pakaian)",
+
+  benda_ibadah: "Kosakata sekitar keagamaan (benda ibadah)",
+  kegiatan_keagamaan: "Kosakata sekitar keagamaan (kegiatan)",
+  peran_keagamaan: "Kosakata sekitar keagamaan (peran)",
+  tempat_ibadah: "Kosakata sekitar keagamaan (tempat ibadah)",
+
+  kejadian: "Kosakata sekitar konsep umum (kejadian)",
+  nilai: "Kosakata sekitar konsep umum (nilai)",
+  status: "Kosakata sekitar konsep umum (status)",
+  waktu: "Kosakata sekitar konsep umum (waktu)",
+
+  alat_dapur: "Kosakata sekitar konsumsi (alat dapur)",
+  bahan_makanan: "Kosakata sekitar konsumsi (bahan makanan)",
+  buah_sayur: "Kosakata sekitar konsumsi (buah & sayur)",
+  makanan: "Kosakata sekitar konsumsi (makanan)",
+  minuman: "Kosakata sekitar konsumsi (minuman)",
+
+  peralatan_olahraga: "Kosakata sekitar olahraga (peralatan)",
+  peran_olahraga: "Kosakata sekitar olahraga (peran)",
+  pertandingan: "Kosakata sekitar olahraga (pertandingan)",
+
+  alat_belajar: "Kosakata sekitar pendidikan (alat belajar)",
+  dokumen: "Kosakata sekitar pendidikan (dokumen)",
+  peran_pendidikan: "Kosakata sekitar pendidikan (peran)",
+  sistem: "Kosakata sekitar pendidikan (sistem)",
+
+  alat_kebersihan: "Kosakata sekitar rumah (alat kebersihan)",
+  penyimpanan: "Kosakata sekitar rumah (penyimpanan)",
+  perabot: "Kosakata sekitar rumah (perabot)",
+  peralatan_rumah: "Kosakata sekitar rumah (peralatan)",
+
+  area: "Kosakata sekitar tempat (area)",
+  bangunan: "Kosakata sekitar tempat (bangunan)",
+  ruangan: "Kosakata sekitar tempat (ruangan)"
 };
